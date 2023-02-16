b9d358c8a65203ac2e2a2a95b19789b34404cbbf
mygit <purceladzedavit@gmail.com>
2023-02-16 16:00:11 +0400

    scenarios when we can use more than one element inside <transition>

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,16 @@
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
   </div>
+  <div class="container">
+    <!--we can use transition wit v-if-else statement when
+    there is 2 element but one of them will be always on UI 
+    mode controls whether firs element has to be animated
+    or leaving element-->
+    <transition name="fade-button" mode="out-in">
+      <button @click="showUsers" v-if="!usersAreVisible">Show users</button>
+      <button @click="hideUsers" v-else>Hide users</button>
+    </transition>
+  </div>
   <!--we can use transition on custom components too -->
   <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
@@ -36,9 +46,16 @@ export default {
       animatedBlock: false,
       dialogIsVisible: false,
       paraIsVisible: false,
+      usersAreVisible: false,
     };
   },
   methods: {
+    showUsers() {
+      this.usersAreVisible = true;
+    },
+    hideUsers() {
+      this.usersAreVisible = false;
+    },
     animateBlock() {
       this.animatedBlock = true;
     },
@@ -102,32 +119,29 @@ button:active {
   animation: slide-fade 0.3s ease-out forwards;
 }
 
-/* vue has default class names  */
-.para-enter-from {
-  /* opacity: 0;
-  transform: translateY(-30px); */
-}
-
 .para-enter-active {
   animation: slide-scale 0.3s ease-out;
 }
 
-.para-enter-to {
-  /* opacity: 1;
-  transform: translateY(0); */
-}
-
-.para-leave-from {
-  /* opacity: 1;
-  transform: translateY(0); */
-}
 .para-leave-active {
   /* transition: all 0.3s ease-in; */
   animation: slide-scale 0.3s ease-out;
 }
-.para-leave-to {
-  /* opacity: 0;
-  transform: translateY(30px); */
+
+.fade-button-enter-from,
+.fade-button-leave-to {
+  opacity: 0;
+}
+.fade-button-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+.fade-button-leave-active {
+  transition: opacity 0.3s ease-in;
+}
+
+.fade-button-enter-to,
+.fade-button-leave-from {
+  opacity: 1;
 }
 
 @keyframes slide-scale {
